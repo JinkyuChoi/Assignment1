249f0b84902c464ba712544ae756e5d0048ec623
Jinkyu Choi <jchoi123@my.centennialcollege.ca>
2019-10-04 17:27:41 -0400

    Map Controller Added
    Map Controller Added

---
+++ Forest Assassin/Assets/_Scripts/MapController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapController : MonoBehaviour
+{
+    public float scrollSpeed;
+    public float resetPosition;
+    public float resetPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        CheckBounds();
+    }
+
+    void Move()
+    {
+        Vector2 newPosition = new Vector2(0.0f, scrollSpeed);
+        Vector2 currentPosition = transform.position;
+
+        currentPosition -= newPosition;
+        transform.position = currentPosition;
+    }
+
+    void Reset()
+    {
+        transform.position = new Vector2(0.0f, resetPosition);
+    }
+
+    void CheckBounds()
+    {
+        if (transform.position.y <= resetPoint)
+        {
+            Reset();
+        }
+    }
+}
